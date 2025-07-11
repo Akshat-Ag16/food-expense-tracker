459a6d0f8a14713c84c776d91d080c616313bea7
Akshat Agarwal <agarwalakshat1611@gmail.com>
2025-07-11 16:51:22 +0530

    Delete categories.py

--- categories.py
+++
@@ -1,5 +0,0 @@
-categories = {"Food and Snacking": ['Amul','Just Chill', 'Dawat', 'GoInsta', '2D', 'TeaPost', 'South Point', 'Atul Bakery', 'Krupa General', 'Hunger Games', 'Tapri', 'Mahavir', 'Outside Restaurant Visit', 'Online food delivery'
-            ],
-            "Pooja Stationery Shop": [],
-            "Cab Rides": ["Ola", "Uber", "Rapido", "Others"],
-            "Reimbursements": ["Amalthea", "Blithchron", 'EII', 'TedX', 'Inter-IIT']}
